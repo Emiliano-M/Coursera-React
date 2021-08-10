a1dccc8668a7c970040527b44632926a3701e5c6
Emiliano <emiliano0montes@gmail.com>
2021-08-09 21:27:37 -0300

    Assigment 1 Done

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -7,20 +7,39 @@ class Details extends Component
     
     render()
     {
-        console.log(this.props);
+        
 
         if(this.props.Dish != null)
         {
+
+            const Comments = this.props.Dish.comments.map((Comment) => {
+                return (
+                    <div>                                          
+                        <CardText>{Comment.comment}</CardText>
+                        <CardText className="mb-3">--{Comment.author}, {Comment.date}</CardText>                                       
+                    </div>
+                );
+            });
+
             return(
-                <Card>
-                    <CardImg src={this.props.Dish.image} alt={this.props.Dish.name} />
-                    <CardBody>
-                        <CardTitle>{this.props.Dish.name}</CardTitle>
-                        <CardText>{this.props.Dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1"> 
+                        <Card>
+                            <CardImg width="100%" src={this.props.Dish.image} alt={this.props.Dish.name} />
+                            <CardBody>
+                                <CardTitle>{this.props.Dish.name}</CardTitle>
+                                <CardText>{this.props.Dish.description}</CardText>
+                            </CardBody>
+                        </Card>                      
+                    </div>  
+                    <div className="col-12 col-md-5 m-1">
+                        <CardTitle>Comments</CardTitle>
+                        {Comments}
+                    </div>
+                </div>
             );
         }
+
         return(
          <div></div>    
         )
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -38,10 +38,8 @@ class Menu extends Component
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Details Dish={this.state.selectedDish}/>
-                    </div>
+                <div>
+                    <Details Dish={this.state.selectedDish}/>   
                 </div>
             </div>
         );
